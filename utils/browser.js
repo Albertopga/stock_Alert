7ab0260385654818c882ad663415d9f64e0dc9c3
Alberto <Albertopga@gmail.com>
2020-12-28 11:04:37 +0100

    .

--- utils/browser.js
+++ utils/browser.js
@@ -2,7 +2,11 @@ const puppeteer = require("puppeteer");
 
 class Browser {
   static async configBrowse(url) {
-    const browser = await puppeteer.launch({ headless: true });
+    const browser = await puppeteer.launch({ 
+      headless: true,
+      args: ["--disable-setuid-sandbox"],
+      'ignoreHTTPSErrors': true 
+    });
     const page = await browser.newPage();
     await page.goto(url);
     return { page, browser };
